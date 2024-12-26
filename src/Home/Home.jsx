8053a45d92e6e5322738f941d5e49d030b7ec724
Naiemjoy1 <naiemunhasan@gmail.com>
2024-12-26 12:09:18 +0600

    section 2 design

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,73 @@
+import { FaRegUser } from "react-icons/fa";
+import { HiMiniCube } from "react-icons/hi2";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { MdFlight, MdFlightTakeoff } from "react-icons/md";
+import { RiPassportLine } from "react-icons/ri";
+import { MdAirlineSeatReclineNormal } from "react-icons/md";
+
 const Home = () => {
   return (
-    <div>
-      <p>Home</p>
+    <div className="bg-gray-300 h-screen flex items-center justify-center">
+      <div className="bg-white w-[80%]  rounded-md">
+        <div className="flex justify-between items-center p-4">
+          <div>
+            <p className="flex gap-2 justify-between items-center">
+              <span>
+                <MdFlight />
+              </span>
+              Flight
+            </p>
+          </div>
+          <div>
+            <div className="flex justify-between items-center gap-4">
+              <p className="flex gap-2 justify-between items-center">
+                <span>
+                  <RiPassportLine />
+                </span>
+                Visa
+              </p>
+              <p className="flex gap-2 justify-between items-center">
+                <span>
+                  <HiMiniCube />
+                </span>
+                Umrah
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-200">
+          <div className="px-4 py-2 flex gap-4">
+            <p className="flex gap-2 items-center">
+              <span>
+                <MdFlightTakeoff />
+              </span>
+              One Way
+              <span>
+                <IoMdArrowDropdown />
+              </span>
+            </p>
+            <p className="flex gap-2 items-center">
+              <span>
+                <FaRegUser />
+              </span>
+              1 Passenger
+              <span>
+                <IoMdArrowDropdown />
+              </span>
+            </p>
+            <p className="flex gap-2 items-center">
+              <span>
+                <MdAirlineSeatReclineNormal />
+              </span>
+              Economy
+              <span>
+                <IoMdArrowDropdown />
+              </span>
+            </p>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
